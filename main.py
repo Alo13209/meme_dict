1e529863fdeed225012a68bcf4b41e7952cc423b
Eloy <tengohambre22314@gmail.com>
2024-12-10 20:34:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 meme_dict ={
     'LOL' : 'Una respuesta a algo gracioso',
     'Red Flag' : 'Alguien con defectos peligrosos',
-    'XD' : 'Expresion a algo gracioso'
-    'LMAO' : 'Expresion a algo muy gracioso'
+    'XD' : 'Expresion a algo gracioso',
+    'LMAO' : 'Expresion a algo muy gracioso',
+    'CRINGE' : 'Sinonimo a verguenza ajena',
+    'RageBait' : 'Molestar alguien con el proposito que se enoje',
+    'Aura' : 'Sinonimo moderno de Cool, pero es algo que se obtiene ( Tengo Aura, Tienes Aura )',
 }
 word = input('Ingresa la palabra a consultar').upper()
 
 if word in meme_dict.keys():
     print('Lo que significa es: ', meme_dict[word])
 else:
-    print('La palabra no existe o esta mal escrita')
+    print('La palabra no existe o esta mal escrita')n
